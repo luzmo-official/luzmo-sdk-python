238397c59cc76f04f7cd087b43d31895b499b4c7
Tuana Celik <tuanacelik95@gmail.com>
2020-10-15 13:48:58 +0100

    complete - ready for testing

--- cumulio/cumulio.py
+++ cumulio/cumulio.py
@@ -17,28 +17,68 @@ class Cumulio(object):
     
     def create(self, resource, properties, associations):
         query = {}
-        query["action"] = "create"
-        query["properties"] = properties
-        query["associations"] = associations
+
+        query["action"]         = "create"
+        query["properties"]     = properties
+        query["associations"]   = associations
         self.__emit(resource, "POST", query)
     
     def get(self, resource, filter):
-        query = {"action": "get", "find": filter}
+        query = {}
+        
+        query["action"] = "get"
+        query["find"]   = filter
         self.__emit(resource, "SEARCH", query)
 
     def delete(self, resource, id, properties = {}):
         query = {}
-        query["action"] = "delete"
-        query["id"] = id
+        
+        query["action"]     = "delete"
+        query["id"]         = id
         query["properties"] = properties
         self.__emit(resource, "DELETE", query)
 
+    def update(self, resource, id, properties = {}):
+        query = {}
+
+        query["action"]     = "update"
+        query["id"]         = id
+        query["properties"] = properties
+        self.__emit(resource, "PATCH", query)
+
+    def associate(self, resource, id, association_role, association_id, properties = {}):
+        query       = {}
+        association = {}
+
+        association["role"] = association_role
+        association["id"]   = association_id
+        query["action"]     = "associate"
+        query["id"]         = id
+        query["resource"]   = association
+        query["properties"] = properties
+        self.__emit(resource, "LINK", query)
     
+    def dissociate(self, resource, id, association_role, association_id):
+        query       = {}
+        association = {}
+
+        association["role"] = association_role
+        association["id"]   = association_id
+        query["action"]     = "associate"
+        query["id"]         = id
+        query["resource"]   = association
+        self.__emit(resource, "UNLINK", query)       
+
+    def query(self, filter = {}):
+        self.get("data", filter)
+
+    def iframe(self, dashboard_id, authorization):
+        return self.APP + "/s/" + dashboard_id + "?key=" + authorization["id"] + "&token=" + authorization["token"]
 
     def __emit(self, resource, action, query):
-        query["key"] = self.api_key
-        query["token"] = self.api_token
-        query["version"] = self.VERSION
+        query["key"]        = self.api_key
+        query["token"]      = self.api_token
+        query["version"]    = self.VERSION
 
         url = self.HOST + ':' + str(self.PORT) + '/' + self.VERSION + '/' + resource
-        x = requests.post(url, headers = {'Content-Type':'application/json'}, data = json.dumps(query))  
+        requests.post(url, headers = {'Content-Type':'application/json'}, data = json.dumps(query))  
